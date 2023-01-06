3af26bb3931c0f31c77c5d7e60bd7c650aae22c6
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-01-06 15:21:41 -0300

    site

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e2212092ed828200"; }
+/******/ 		__webpack_require__.h = function() { return "8e232dd38dd848de"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/CardDestaques.jsx
+++ components/CardDestaques.jsx
@@ -2,13 +2,14 @@ import Image from "next/image"
 import fotoCard from "../public/bgCard.jpg"
 import iconCoracao from "../public/icons/coracao.png"
 import iconMais from "../public/icons/mais.png"
+import flecha from "../public/icons/flecha.png"
 
 
 export default function CardDestaques({ imovel, cidade, quartos, metros, preco, }) {
     return (
         <div className=" space-x-9 flex flex-row shadow m-auto">
             <div className="w-72 flex flex-col justify-start text-start bg-gray-300 rounded-md pb-5">
-                <Image src={fotoCard} alt='imagem do card' className="rounded-md mb-4"/>
+                <Image src={fotoCard} alt='imagem do card' className="rounded-md mb-4" />
                 <div className="mx-4 ">
                     <p className="text-xl text-gray-800 font-bold mb-5 ">
                         {imovel}
@@ -16,8 +17,11 @@ export default function CardDestaques({ imovel, cidade, quartos, metros, preco, 
                     <p className="text-gray-600">
                         {cidade}
                     </p>
-                    <div className="flex justify-between mx-4 mb-7 text-gray-800">
-                        <p>{quartos}</p>
+                    <div className="flex justify-between mx-4 mb-7 text-gray-800 mt-2">
+                        <p className="flex"> <Image src={flecha}
+                            className="mr-4 w-5 h-auto opacity-70" />
+                            {quartos}
+                        </p>
                         <p>{metros}</p>
                     </div>
                     <div className="flex justify-between">
